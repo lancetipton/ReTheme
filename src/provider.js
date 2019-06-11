8e1b6da8d72a4c6961718d6eccb91cb1243e2ac1
lancetipton <lancetipton04@gmail.com>
2019-06-11 06:17:04 -0700

    Removed proptypes dep, incase not install in source project.

--- src/provider.js
+++ src/provider.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import ThemeContext from './context'
 
 export default class ReThemeProvider extends Component {
-
-  static propTypes = {
-    children: PropTypes.node,
-  }
 
   render() {
     const { children, value } = this.props
